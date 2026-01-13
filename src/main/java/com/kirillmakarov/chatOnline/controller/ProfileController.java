6232a47b2978f3db5a1e01ef65acbf22a5d0200e
Kirill <kiryusha.makarov.04@mail.ru>
2026-01-13 20:13:27 +0300

    Revert "Fix problem with npm and web-sockets and fix design of chats"
    This reverts commit 9d578e83100dc5f4e45f6cc3fb261d9a996d0874.

--- src/main/java/com/kirillmakarov/chatOnline/controller/ProfileController.java
+++ src/main/java/com/kirillmakarov/chatOnline/controller/ProfileController.java
@@ -42,8 +42,8 @@ public class ProfileController {
         userService.saveUserProfileSettings(userDto, file, curUsername);
         return "redirect:/profile";
     }
-// Make url without users
-    @GetMapping("/{username}")
+
+    @GetMapping("/users/{username}")
     public String viewUserProfile(@PathVariable String username,
                                   Model model,
                                   Principal principal){
